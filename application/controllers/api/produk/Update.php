17a6ca66a0a1d5ec0bbbcc18700ad1db01a123fe
voltgizerz <felix.asui@gmail.com>
2020-04-06 22:10:44 +0700

    FIX UPDATE GAMBAR PRODUK

--- application/controllers/api/produk/Update.php
+++ application/controllers/api/produk/Update.php
@@ -15,6 +15,8 @@ class Update extends REST_Controller
         $this->load->model('Produk_model');
     }
 
+  
+
     public function index_post($id = null)
     {
         date_default_timezone_set("Asia/Bangkok");
@@ -22,7 +24,7 @@ class Update extends REST_Controller
         $produk->nama_produk = $this->post('nama_produk');
         $produk->harga_produk = $this->post('harga_produk');
         $produk->stok_produk = $this->post('stok_produk');
-        $produk->gambar_produk = $this->response_upload();
+        $produk->gambar_produk = $this->response_upload($id);
         $produk->stok_minimal_produk = $this->post('stok_minimal_produk');
 
         $produk->updated_date = date("Y-m-d H:i:s");
@@ -33,36 +35,43 @@ class Update extends REST_Controller
         return $this->returnData($response['msg'], $response['error']);
     }
 
-    public function response_upload()
+    public function response_upload($id)
     {
-        $this->load->helper(array('form', 'url'));
-
-        $config = array(
-            'upload_path' => "upload/gambar_produk/",
-            'allowed_types' => "gif|jpg|png|jpeg",
-            'overwrite' => true,
-            'encrypt_name' => true,
-            'max_size' => "2048000",
-            'max_height' => "3000",
-            'max_width' => "3000",
-        );
-        
-        if (!file_exists('upload/gambar_produk/')) {
-            mkdir('upload/gambar_produk/', 0777, true);
+        $part = "upload/gambar_produk/";
+	    $filename = "img".rand(9,9999).".jpg";
+	    
+	    if (!file_exists('upload/gambar/')) {
+            mkdir('upload/gambar/', 777, true);
         }
+        
+         
+       if ($_FILES["gambar_produk"]["name"] != "") 
+       {
+          
+            $destinationfile = $part.$filename;
+			if(move_uploaded_file($_FILES['gambar_produk']['tmp_name'],  $destinationfile))
+			{
+                return $destinationfile;
+			}else
+			{
+			    // gagal upload
+			    return 'upload/gambar_produk/default.jpg' ;
+			}
 
-        $this->load->library('upload', $config);
-
-        if ($this->upload->do_upload('gambar_produk')) {
-            $upload_data = $this->upload->data(); //Returns array of containing all of the data related to the file you uploaded.
-            $file_name = $upload_data['file_name'];
-
-            $dir_img = "upload/gambar_produk/" . $file_name;
-            return $dir_img;
-        } else {
-            return 'upload/gambar_produk/default.jpg';
+        } 
+        else 
+        {
+            //FILE TIDAK ADA DI UPLOAD
+            if($this->Produk_model->cekGambar($id) == 1){
+                return 'upload/gambar_produk/default.jpg';
+            }else{
+                
+                return $this->Produk_model->cekGambar($id);
+            }
+         ;
         }
     }
+
 
     public function returnData($msg, $error)
     {
