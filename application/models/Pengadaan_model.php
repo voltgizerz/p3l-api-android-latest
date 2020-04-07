136e357708e47d2883b22a54f16e405bfa40c226
voltgizerz <felix.asui@gmail.com>
2020-04-07 20:32:59 +0700

    FIX GET PENGADAAN BELUM KELAR

--- application/models/Pengadaan_model.php
+++ application/models/Pengadaan_model.php
@@ -7,11 +7,28 @@ class Pengadaan_model extends CI_Model
     {
         if ($id === null) {
 
-            return $this->db->get('data_pengadaan')->result_array();
+            $this->db->select('data_detail_pengadaan.kode_pengadaan_fk,data_detail_pengadaan.id_produk_fk,data_detail_pengadaan.satuan_pengadaan,data_detail_pengadaan.jumlah_pengadaan');
+            $this->db->join('data_pengadaan', 'data_pengadaan.kode_pengadaan = data_detail_pengadaan.kode_pengadaan_fk');
+            $this->db->from('data_detail_pengadaan');
+
+            $queryDetail = $this->db->get();
+            $arrTempDetail = $queryDetail->result_array();
+
+            $this->db->select('kode_pengadaan,id_supplier,status,tanggal_pengadaan,total');
+            $this->db->from('data_pengadaan');
+            $query = $this->db->get();
+            $arrTemp = $query->result_array();
+
+            for ($i =0; $i < count($arrTemp); $i++) {
+                $arrTemp[$i]['produk_dibeli'] = $arrTempDetail;
+            }
+
+            return $arrTemp;
+
             # code...
         } else {
-
             $this->db->where('kode_pengadaan', $id);
+
             return $this->db->get('data_pengadaan')->result_array();
         }
     }
@@ -34,9 +51,7 @@ class Pengadaan_model extends CI_Model
             'id_supplier' => $request->id_supplier,
             'status' => $request->status,
             'tanggal_pengadaan' => $request->tanggal_pengadaan,
-            'total'=> $request->total,
-            'updated_date' => $request->updated_date,
-            'deleted_date' => $request->deleted_date,
+            'total' => $request->total,
         ];
 
         if ($this->db->where('kode_pengadaan', $id)->update('data_pengadaan', $updateData)) {
