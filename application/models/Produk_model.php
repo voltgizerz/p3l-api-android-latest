ec429d554bc056095144e38c59b7bab3f59aa011
voltgizerz <felix.asui@gmail.com>
2020-04-19 15:01:41 +0700

    DONE SOFT DELETE PRODUK

--- application/models/Produk_model.php
+++ application/models/Produk_model.php
@@ -35,14 +35,59 @@ class Produk_model extends CI_Model
     public function deleteProduk($id)
     {
         $this->db->db_debug = false;
-        $this->db->delete('data_produk', ['id_produk' => $id]);
-        $rowAffected = $this->db->affected_rows();
-        $e = $this->db->error();
+        //TAMPUNG SEMENTARA DATA YANG KEMUNGKINAN BISA DIHAPUS
+        $this->db->select('*');
+        $this->db->from('data_produk');
+        $this->db->where('id_produk', $id);
+        $query = $this->db->get();
+        $arrTampData = $query->result_array();
 
-        if ($e['code'] == 1451) {
-            return -1;
+        if ($this->db->delete('data_produk', ['id_produk' => $id]) == false) {
+            //INI JIKA DATA INI SEDANG DIGUNAKAN
+            $rowAffected = $this->db->affected_rows();
+            $e = $this->db->error();
+
+            if ($e['code'] == 1451) {
+                return -1;
+            } else {
+                return $rowAffected;
+            }
         } else {
-            return $rowAffected;
+            // DATA BERHASIL DI HAPUS BERARTI TIDAK SEDANG DIGUNAKAN
+           
+            $data = [
+                'id_produk' => $arrTampData[0]['id_produk'],
+                'nama_produk' => $arrTampData[0]['nama_produk'],
+                'harga_produk' => $arrTampData[0]['harga_produk'],
+                'stok_produk' => $arrTampData[0]['stok_produk'],
+                'gambar_produk' => $arrTampData[0]['gambar_produk'],
+                'gambar_produk_desktop' =>  file_get_contents($arrTampData[0]['gambar_produk']),
+                'stok_minimal_produk' => $arrTampData[0]['stok_minimal_produk'],
+                'created_date' => $arrTampData[0]['created_date'],
+                'updated_date' => $arrTampData[0]['updated_date'],
+                'deleted_date' => $arrTampData[0]['deleted_date'],
+            ];
+            
+            // RETURN DATA
+            $this->db->insert('data_produk', $data);
+            date_default_timezone_set("Asia/Bangkok");
+            // INSERT DELETE AT DAN UPDATE DATA
+            $updateData =
+                ['created_date' => date("0000:00:0:00:00"),
+                'deleted_date' => date("Y-m-d H:i:s"),
+            ];
+
+            $this->db->where('id_produk', $id);
+            $this->db->update('data_produk', $updateData);
+            $rowAffected = $this->db->affected_rows();
+
+            $e = $this->db->error();
+
+            if ($e['code'] == 1451) {
+                return -1;
+            } else {
+                return $rowAffected;
+            }
         }
     }
     public function createProduk($data)
@@ -58,7 +103,7 @@ class Produk_model extends CI_Model
             'harga_produk' => $request->harga_produk,
             'stok_produk' => $request->stok_produk,
             'gambar_produk' => $request->gambar_produk,
-            'gambar_produk_desktop' =>$request->gambar_produk_desktop,
+            'gambar_produk_desktop' => $request->gambar_produk_desktop,
             'stok_minimal_produk' => $request->stok_minimal_produk,
             'updated_date' => $request->updated_date,
             'deleted_date' => $request->deleted_date,
@@ -99,6 +144,21 @@ class Produk_model extends CI_Model
         } else {
             return $gambar;
         }
+
+    }
+
+    public function restoreProduk($request, $id)
+    {
+        $updateData =
+            [
+            'created_date' => $request->created_date,
+            'deleted_date' => $request->deleted_date,
+        ];
+
+        if ($this->db->where('id_produk', $id)->update('data_produk', $updateData)) {
+            return ['msg' => 'SUKSES RESTORE PRODUK!', 'id_produk' => $id, 'error' => false];
+        }
+        return ['msg' => 'GAGAL, RESTORE PRODUK ID TIDAK DITEMUKAN !', 'error' => true];
 
     }
 
